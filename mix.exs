3974f0291ec6636f95a014dc1c4c5c16ba205fd8
Reio Piller <reio@heathmont.net>
2019-09-18 19:21:46 +0300

    version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PhoenixViewAssets.MixProject do
   def project do
     [
       app: :phoenix_view_assets,
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       package: package(),
